b36694b28796b7c196c9ca3d66ff4a9bd33a872a
TurNieruchomosci <aleh.tur@tur-nieruchomosci.pl>
2025-03-30 15:29:48 +0200

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,10 @@
+import { AsariCrm } from './nodes/AsariCrm.node';
+import { AsariCrmApi } from './credentials/AsariCrmApi.credentials';
+
+export const nodes = [
+  AsariCrm,
+];
+
+export const credentials = [
+  AsariCrmApi,
+];
